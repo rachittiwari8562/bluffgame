7eef558795584a04caa614854e5c7a36251bf2f7
rachit <rachit8562@gmail.com>
2020-09-13 20:33:30 +0530

    changed settings.py

--- chatting/settings.py
+++ chatting/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3)jy6mf%#@apop4(5^ks)fsyrfytt0w9%yql$^!&iswpuso*d('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blufgame.herokuapp.com']
 
 
 # Application definition
