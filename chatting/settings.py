69dee6bb117b7ebb4db03a4c405810cf7b653868
rachit <rachit8562@gmail.com>
2020-09-13 21:12:54 +0530

    changing

--- chatting/settings.py
+++ chatting/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3)jy6mf%#@apop4(5^ks)fsyrfytt0w9%yql$^!&iswpuso*d('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['blufgame.herokuapp.com']
+ALLOWED_HOSTS = ['blufgame.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -128,5 +128,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'chat','static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
